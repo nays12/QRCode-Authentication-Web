63bb9457b98fd91d76604978ab0ced14a6c1da32
Naomi Wiggins <naomiwiggins08@gmail.com>
2019-10-28 12:23:16 -0500

    Added model class to dbContext file

--- QRCodeAuth-Web/QRCodeAuth-Web/Models/WebData.cs
+++ QRCodeAuth-Web/QRCodeAuth-Web/Models/WebData.cs
@@ -1,7 +1,8 @@
 namespace QRCodeAuth_Web.Models
 {
 	using System;
-	using System.Data.Entity;
+    using System.Collections.Generic;
+    using System.Data.Entity;
 	using System.Linq;
 
 	public class WebData : DbContext
@@ -12,20 +13,51 @@ namespace QRCodeAuth_Web.Models
 		// 
 		// If you wish to target a different database and/or database provider, modify the 'WebData' 
 		// connection string in the application configuration file.
+
 		public WebData()
 			: base("name=WebData")
 		{
 		}
 
-		// Add a DbSet for each entity type that you want to include in your model. For more information 
-		// on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-		// public virtual DbSet<MyEntity> MyEntities { get; set; }
+		// DbSets
+		public DbSet<User> Users { get; set; }
+		public DbSet<Account> Accounts { get; set; }
+		public DbSet<Event> Events { get; set; }
 	}
 
-	//public class MyEntity
-	//{
-	//    public int Id { get; set; }
-	//    public string Name { get; set; }
-	//}
+	public class User
+	{
+		public int user_id { get; set; }
+		public string first_name { get; set; }
+		public string last_name { get; set; }
+		public string group { get; set; }
+		public List<Account> accounts { get; set; }
+	}
+
+	public class Account
+	{
+		public int account_id { get; set; }
+		public User owner { get; set; }
+		public string type { get; set; }
+		public string department_id { get; set; }
+		public string department_name { get; set; }
+		public bool is_active { get; set; }
+		public bool is_credential_authority { get; set; }
+		public bool is_attendance_manager { get; set; }
+		public bool is_information_collector { get; set; }
+		public Event events { get; set; }
+	}
+
+	public class Event
+	{
+		public int event_id { get; set; }
+		public Account owner { get; set; }
+		public string name { get; set; }
+		public string type { get; set; }
+		public string details { get; set; }
+		public DateTime start_time { get; set; }
+		public DateTime end_time { get; set; }
+		public string credentials_needed { get; set; }
+		public List<Account> attendees { get; set; }
+	}
 }
