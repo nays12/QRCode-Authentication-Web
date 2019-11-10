53386aa9b3ea3438be372cb49295169ae68847b2
gaozhihaoid <56491017+gaozhihaoid@users.noreply.github.com>
2019-11-09 23:50:53 -0600

    Add random number function at default page.

--- QRCodeAuth-Web/QRCodeAuth-Web/Default.aspx.cs
+++ QRCodeAuth-Web/QRCodeAuth-Web/Default.aspx.cs
@@ -7,14 +7,30 @@
  */
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace QRCodeAuth_Web
 {
-	public partial class Default : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-		}
-	}
+            Random randrom = new Random((int)DateTime.Now.Ticks);
+
+            string str = "";
+            for (int i = 0; i < 10; i++)
+            {
+                str += chars[randrom.Next(chars.Length)];
+            }
+
+            //test random number
+            //Response.Write(str);
+            //Console.Read();
+        }
+    }
+
 }
