cf2131b7a9eb85d3b8ddbc85077624b0cc968965
ortuno.marilin@outlook.com <ortuno.marilin@outlook.com>
2019-11-10 16:17:12 -0600

    Created OTPController and OTP model class for random 6-digit code.

---
+++ QRCodeAuth-Web/QRCodeAuth-Web/Controllers/OTPController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using QRCodeAuth_Web.Models;
+
+namespace QRCodeAuth_Web.Controllers
+{
+    public class OTPController : ApiController
+    {
+        // GET: api/OTP
+        [Route("api/OTP")]
+        [HttpGet]
+        public string Value()
+        {
+            return OTP.generateOTP();
+        }
+    }
+}
---
+++ QRCodeAuth-Web/QRCodeAuth-Web/Models/OTP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QRCodeAuth_Web.Models
+{
+    public class OTP
+    {
+
+        public OTP()
+        {
+
+        }
+
+        public static string generateOTP()
+        {
+            string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            Random randrom = new Random((int)DateTime.Now.Ticks);
+
+            string str = "";
+            for (int i = 0; i < 10; i++)
+            {
+                str += chars[randrom.Next(chars.Length)];
+            }
+
+            return str;
+        }
+    }
+}
