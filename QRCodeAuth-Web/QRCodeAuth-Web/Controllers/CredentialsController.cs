1f0c62081dc91bf701dee5aa256c9d9e90e7bc1a
ortuno.marilin@outlook.com <ortuno.marilin@outlook.com>
2019-11-16 17:11:36 -0600

    Updated Credentials Controller

--- QRCodeAuth-Web/QRCodeAuth-Web/Controllers/CredentialsController.cs
+++ QRCodeAuth-Web/QRCodeAuth-Web/Controllers/CredentialsController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,33 +15,105 @@ namespace QRCodeAuth_Web.Controllers
 {
     public class CredentialsController : ApiController
     {
-
         private WebSystemData db = new WebSystemData();
 
         // GET: api/Credentials
-        [Route("api/Credentials")]
-        [HttpGet]
-        public string GetCredentials()
+        public IQueryable<Credential> GetCredentials()
         {
-            //return db.Credentials;
-            return "Returns all credentials";
+            return db.Credentials;
         }
 
         // GET: api/Credentials/5
         [ResponseType(typeof(Credential))]
         public IHttpActionResult GetCredential(int id)
         {
-            Credential @Credential = db.Credentials.Find(id);
-            if (@Credential == null)
+            Credential credential = db.Credentials.Find(id);
+            if (credential == null)
             {
                 return NotFound();
             }
 
-            return Ok(@Credential);
+            return Ok(credential);
         }
 
+        // PUT: api/Credentials/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCredential(int id, Credential credential)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            if (id != credential.Id)
+            {
+                return BadRequest();
+            }
 
+            db.Entry(credential).State = EntityState.Modified;
 
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CredentialExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Credentials
+        [ResponseType(typeof(Credential))]
+        public IHttpActionResult PostCredential(Credential credential)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Credentials.Add(credential);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = credential.Id }, credential);
+        }
+
+        // DELETE: api/Credentials/5
+        [ResponseType(typeof(Credential))]
+        public IHttpActionResult DeleteCredential(int id)
+        {
+            Credential credential = db.Credentials.Find(id);
+            if (credential == null)
+            {
+                return NotFound();
+            }
+
+            db.Credentials.Remove(credential);
+            db.SaveChanges();
+
+            return Ok(credential);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CredentialExists(int id)
+        {
+            return db.Credentials.Count(e => e.Id == id) > 0;
+        }
     }
 }
