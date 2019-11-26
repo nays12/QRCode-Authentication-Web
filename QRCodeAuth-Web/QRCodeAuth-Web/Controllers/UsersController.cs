5dd7c5fe98e0bd6cd729aaf4db117aa83f760694
nays12 <41583144+nays12@users.noreply.github.com>
2019-11-26 14:42:03 -0600

    Added controller that get User's info from database before creating account

---
+++ QRCodeAuth-Web/QRCodeAuth-Web/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using QRCodeAuth_Web.Data;
+using QRCodeAuth_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace QRCodeAuth_Web.Controllers
+{
+	public class UsersController : ApiController
+	{
+
+		[Route("api/Users/GetUserWithId")]
+		[HttpGet]
+		[ResponseType(typeof(User))]
+		public User GetUserWithId(string id)
+		{
+			return UsersRepo.FindUserById(id);
+		}
+
+	}
+}
